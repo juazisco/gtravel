22bdbc3852dcde7a4788dced3804e2e88a333e07
Francisco Quiñones Enciso <joseq@apesol.org.pe>
2015-03-16 00:07:58 -0500

    delete duplicate files

--- gtravel/gtravel/events/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from events.views import event_view
-from applications.views import manage_application
-
-urlpatterns = patterns('',
-    url(r'^(?P<event_id>\d+)/$', event_view, name='detail'),
-    url(r'^(?P<event_id>\d+)/apply$', manage_application, name='detail'),
-)
